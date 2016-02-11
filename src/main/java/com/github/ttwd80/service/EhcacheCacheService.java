b696380aba9a9b7c8ee76c98b36af314216670ff
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2016-02-11 15:15:22 +1100

    native service code for cache

--- src/main/java/com/github/ttwd80/service/EhcacheCacheService.java
+++ src/main/java/com/github/ttwd80/service/EhcacheCacheService.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
+
+import net.sf.ehcache.Ehcache;
 
 @Service
 public class EhcacheCacheService implements CacheService {
@@ -13,9 +16,24 @@ public class EhcacheCacheService implements CacheService {
 	@Autowired
 	CacheManager cacheManager;
 
+	private final String cacheName = "default-cache";
+
 	@Override
 	public List<String> listKeys() {
-		return new ArrayList<>(cacheManager.getCacheNames());
+		List<?> list = getNativeCache().getKeys();
+		List<String> keys = new ArrayList<>();
+		for (Object object : list) {
+			keys.add(object.toString());
+		}
+		return keys;
+	}
+
+	private Cache getCache() {
+		return cacheManager.getCache(cacheName);
+	}
+
+	private Ehcache getNativeCache() {
+		return (Ehcache) getCache().getNativeCache();
 	}
 
 }
